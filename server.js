11f091676ef58c32820de7b7cf4f9a2c7f3d82a7
khalilsaw <khalilfar720@gmail.com>
2023-02-18 21:11:37 +0000

    port fix

--- server.js
+++ server.js
@@ -249,7 +249,7 @@ io.on('connection', (socket) => {
 });
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
