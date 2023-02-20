12a4c449e0ea64a6378b5a6284f7d916c1abead8
khalilsaw <khalilfar720@gmail.com>
2023-02-20 11:18:10 +0000

    prettier , login , css modif

--- server.js
+++ server.js
@@ -1,62 +1,55 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const http = require('http');
-const { clippingParents } = require('@popperjs/core');
+const http = require("http");
+const { clippingParents } = require("@popperjs/core");
 const server = http.createServer(app);
-const io = require('socket.io')(server);
-const adminRole = 'admin';
-const arbitreRole = 'arbitre';
+const io = require("socket.io")(server);
+const adminRole = "admin";
+const arbitreRole = "arbitre";
 
 let rooms = [];
 const users = new Map();
-
 
 let numConnectedArbitr = 0; // Counter variable to track the number of connected arbitr
 const maxConnections = 5;
 let numConnectedAdmin = 0; // Counter variable to track the number of connected admin clients
 const maxAdminConnections = 1; // Maximum number of connections for admin clients
 
+app.use("/img/", express.static(path.join(__dirname, "img")));
+app.use("/socket.io", express.static(path.join(__dirname, "socket.io")));
+app.use("/sound", express.static(path.join(__dirname, "sound")));
+app.use("/css", express.static(path.join(__dirname, "css")));
 
-
-
-app.use('/img/', express.static(path.join(__dirname, 'img')));
-app.use('/socket.io', express.static(path.join(__dirname,'socket.io')));
-app.use('/sound', express.static(path.join(__dirname, 'sound')));
-app.use('/css', express.static(path.join(__dirname, 'css')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get('/:username/:role/:roomid', (req, res) => {
+app.get("/:username/:role/:roomid", (req, res) => {
   const { username, role, roomid } = req.params;
   switch (role) {
-    case 'admin':
-      res.sendFile(path.join(__dirname, 'admin.html'));
+    case "admin":
+      res.sendFile(path.join(__dirname, "admin.html"));
       break;
-    case 'arbitre':
-      res.sendFile(path.join(__dirname, 'arbitre.html'));
+    case "arbitre":
+      res.sendFile(path.join(__dirname, "arbitre.html"));
       break;
-    case 'tv':
-      res.sendFile(path.join(__dirname, 'tv.html'));
+    case "tv":
+      res.sendFile(path.join(__dirname, "tv.html"));
       break;
-    case 'winner':
-      res.sendFile(path.join(__dirname, 'winner.html'));
+    case "winner":
+      res.sendFile(path.join(__dirname, "winner.html"));
       break;
-    case 'tvplayers':
-      res.sendFile(path.join(__dirname, 'tvplayers.html'));
+    case "tvplayers":
+      res.sendFile(path.join(__dirname, "tvplayers.html"));
       break;
     default:
-      res.status(404).send('Invalid role specified');
+      res.status(404).send("Invalid role specified");
       break;
   }
 });
 
-
-
-
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   // Declare the values object for this connection
   /* let values = {
     1: [],
@@ -68,30 +61,27 @@ io.on('connection', (socket) => {
   let updatedValues = {
     1: [],
     2: [],
-    3: []
+    3: [],
   };
   // check rooms
-  socket.on('check-room', ({ roomid, role }, callback) => {
+  socket.on("check-room", ({ roomid, role }, callback) => {
     // Check whether the room exists
     // Your code for checking the room can go here
     const roomExists = rooms.includes(roomid); // set this variable to true or false based on your check
     callback({ exists: roomExists });
-
-    
-    
   });
 
   // store rooms
-  socket.on('generated-room-stored',(roomid)=>{
+  socket.on("generated-room-stored", (roomid) => {
     rooms.push(roomid);
   });
 
   // when a user gets connected
-  socket.on('login', ({ username, role, roomid }) => {
-
-    console.log(`${username} connected to server as ${role} in room ${roomid} //////// ${socket.id}`);
+  socket.on("login", ({ username, role, roomid }) => {
+    console.log(
+      `${username} connected to server as ${role} in room ${roomid} //////// ${socket.id}`
+    );
     console.log(users);
-
 
     // Add the user to the specified room
     socket.join(roomid);
@@ -100,107 +90,105 @@ io.on('connection', (socket) => {
     users.set(username, { id: socket.id, role, roomid });
 
     // Broadcast a message to all connected users in the room
-    io.to(roomid).emit('message', `${username} (${role}) has joined the chat in room ${roomid}`);
-    
-    socket.on('send-value', (value, roomid) => {
+    io.to(roomid).emit(
+      "message",
+      `${username} (${role}) has joined the chat in room ${roomid}`
+    );
+
+    socket.on("send-value", (value, roomid) => {
       console.log("Value received: " + JSON.stringify(value));
-    
+
       const round = value.round;
       const arbitreName = value.arbitre;
       const winner = value.winner;
-      console.log(round +" "+ arbitreName +" "+ winner +" from the server")
-              
-      io.to(roomid).emit('receive-values',{ round, arbitreName, winner });
+      console.log(
+        round + " " + arbitreName + " " + winner + " from the server"
+      );
+
+      io.to(roomid).emit("receive-values", { round, arbitreName, winner });
     });
 
     //redirection code  for timer
 
-    socket.on('start-timer-admin', ()=>{
-      console.log("timer in server changed")      
-      io.to(roomid).emit('start-timer');
+    socket.on("start-timer-admin", () => {
+      console.log("timer in server changed");
+      io.to(roomid).emit("start-timer");
     });
 
-    socket.on('pause-timer-admin', ()=>{
-      console.log("timer in server changed" )
-      io.to(roomid).emit('pause-timer');
+    socket.on("pause-timer-admin", () => {
+      console.log("timer in server changed");
+      io.to(roomid).emit("pause-timer");
     });
-    socket.on('restart-timer-admin', ()=>{
-      console.log("timer in server changed" )
-      io.to(roomid).emit('restart-timer');
+    socket.on("restart-timer-admin", () => {
+      console.log("timer in server changed");
+      io.to(roomid).emit("restart-timer");
     });
 
     // Listen for the 'select-round' event
-    socket.on('select-round', (round) => {
+    socket.on("select-round", (round) => {
       console.log(`Admin selected round ${round}`);
 
       // Update the values object with the selected round
       values = {
-        1: round === '1' ? values[1] : values[1],
-        2: round === '2' ? values[2] : values[2],
-        3: round === '3' ? values[3] : values[3]
+        1: round === "1" ? values[1] : values[1],
+        2: round === "2" ? values[2] : values[2],
+        3: round === "3" ? values[3] : values[3],
       };
 
       // Emit an event to the arbitre client with the selected round value
 
-      io.to(roomid).emit('selected-round', round);
+      io.to(roomid).emit("selected-round", round);
     });
-
-
-
 
     //redirect url to winner
 
-    socket.on('redirect-player-to-tv' , (url3)=>{
-      io.to(roomid).emit('redirect-player-to-tv-from-server' , url3);
-      console.log(url3)
+    socket.on("redirect-player-to-tv", (url3) => {
+      io.to(roomid).emit("redirect-player-to-tv-from-server", url3);
+      console.log(url3);
     });
 
-    socket.on('redirect-tv-to-winner', (url)=>{
-      io.to(roomid).emit('redirect-tv-to-winner-from-server' , url);
-      console.log(url)
+    socket.on("redirect-tv-to-winner", (url) => {
+      io.to(roomid).emit("redirect-tv-to-winner-from-server", url);
+      console.log(url);
     });
 
-    socket.on('redirect-tv-to-players', (url2)=>{
-      io.to(roomid).emit('redirect-tv-to-players-from-server' , url2);
-      console.log(url2)
+    socket.on("redirect-tv-to-players", (url2) => {
+      io.to(roomid).emit("redirect-tv-to-players-from-server", url2);
+      console.log(url2);
     });
 
     //outcard event
     socket.on("outcard", (winnercolor) => {
-      console.log("out card service "+ winnercolor);
-      io.to(roomid).emit("outcardV2",winnercolor);
-    })
+      console.log("out card service " + winnercolor);
+      io.to(roomid).emit("outcardV2", winnercolor);
+    });
 
-
-
-  
     // Decrement the counter when an arbitrator disconnects
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
       const user = users[socket.id];
       if (user && user.role === arbitreRole) {
         numConnectedArbitr--;
-        console.log(`Arbitrator ${socket.id} disconnected. ${numConnectedArbitr} arbitrators remaining.`);
-      } else if (user && user.role === 'admin') {
+        console.log(
+          `Arbitrator ${socket.id} disconnected. ${numConnectedArbitr} arbitrators remaining.`
+        );
+      } else if (user && user.role === "admin") {
         numConnectedAdmin--;
-        console.log(`Admin ${socket.id} disconnected. ${numConnectedAdmin} admins remaining.`);
+        console.log(
+          `Admin ${socket.id} disconnected. ${numConnectedAdmin} admins remaining.`
+        );
       } else {
         console.log(`User ${socket.id} disconnected.`);
       }
 
       delete users[socket.id];
-      
+
       // Leave the room the user joined
       socket.leave(roomid);
-
     });
-
   });
 });
-
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
-
